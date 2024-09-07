8e32276e482ce6454913fbbbe32a04d18d9fc05c
i-ma-i <162835512+i-ma-i@users.noreply.github.com>
2024-09-08 01:33:53 +0900

    再開機能作成

--- Assets/Scripts/Breakout.cs
+++ Assets/Scripts/Breakout.cs
@@ -1,7 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Breakout : MonoBehaviour
 {
+    Ball m_ball;
+    float m_deadLine;
+    bool m_playing;
+
+    private void Start()
+    {
+        m_ball = FindObjectOfType<Ball>();
+        m_deadLine = FindObjectOfType<Bar>().transform.position.y - 1;
+        m_playing = true;
+    }
+
+    private void Update()
+    {
+        if(m_playing == false)
+        {
+            return;
+        }
+
+        if (m_ball.transform.position.y < m_deadLine)
+        {
+            Destroy(m_ball.gameObject);
+            m_playing = false;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
     public void Exit()
     {
         Application.Quit();
